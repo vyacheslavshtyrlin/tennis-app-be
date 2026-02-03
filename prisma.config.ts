e3d2b82156df74bd9d5dacc0634896195474489a
VyacheslavShtyrlin <georgesescoffierauguste@gmail.com>
2026-02-04 00:55:52 +0500

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,4 @@
-import { config } from 'dotenv';
 import { defineConfig, env } from 'prisma/config';
-
-config({
-  path: process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.example',
-});
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
